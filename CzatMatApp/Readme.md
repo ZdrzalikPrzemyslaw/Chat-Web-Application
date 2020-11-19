1b4f28fc10c2e86a9600331a04df5bde9c8b8f13
224466 Zdrzalik Przemyslaw <57731778+zdrzalik-przemek@users.noreply.github.com>
2020-11-19 20:13:27 +0100

    our app : D

---
+++ CzatMatApp/Readme.md
@@ -0,0 +1,7 @@
+To deploy site download Azure Console.
+
+Use az login to login into console.
+
+Use mvn clean package azure-webapp:deploy to, deploy to the cloud.
+
+Use mvn com.microsoft.azure:azure-webapp-maven-plugin:1.12.0:config to change the cloud config
---
+++ CzatMatApp/src/main/java/tech/czatmat/app/HelloController.java
@@ -0,0 +1,14 @@
+package tech.czatmat.app;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+	@RequestMapping("/")
+	public String index() {
+		return "Siema Dzulka";
+	}
+
+}
