08f49e3bf6574209bb0ca0fe524e179f028b1b7e
224441 Szymańska Julia <224441@edu.p.lodz.pl>
2020-11-23 12:45:59 +0100

    Added chat lists

--- FrontEnd/src/App.vue
+++ FrontEnd/src/App.vue
@@ -1,15 +1,18 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+<!--  <img alt="Vue logo" src="./assets/logo.png">-->
+<!--  <HelloWorld msg="Welcome to Your Vue.js App"/>-->
+  <ChatsList/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
+import ChatsList from "@/components/ChatsList";
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    // HelloWorld,
+    ChatsList,
   }
 }
 </script>
--- FrontEnd/src/components/ChatsList.vue
+++ FrontEnd/src/components/ChatsList.vue
@@ -1,13 +1,49 @@
 <template>
-
+  <div class="container">
+    <div class="row" v-for="chat in this.chats" v-bind:key="chat" id="OneChat" @click="this.onClickChat">
+      <div class="col" id="UserName">
+        {{ chat[0] }}
+      </div>
+      <div class="col" id="LastMessage">
+        {{ chat[1] }}
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
-  name: "ChatsList"
+  name: "ChatsList",
+  data() {
+    return {
+      chats: [["Julia Szymanska", "Dobranoc"], ["Martyna Piasecka", "Do zobaczenia"], ["Przemyslaw Zdrzalik", "Zrobimy to jutro!"], ["Huber Gawłowski", "Kiedy wychodzimy?"], ["Kamil Kiszko-Zgierski", "Ok"]],
+    }
+  },
+  methods: {
+    onClickChat: function () {
+    //    Tutaj trzeba będzie zrobić metodę onClick
+    }
+  }
 }
 </script>
 
 <style scoped>
+#OneChat {
+  padding: 15px;
+  text-align: left;
+  cursor: pointer;
+  width: 10%;
+  background: #D4D4D4;
+  margin: 2px;
+  border-radius: 10px;
+}
 
+#UserName {
+  font-weight: bold;
+  padding: 2px 0
+}
+
+#LastMessage {
+  opacity: 0.9;
+}
 </style>
