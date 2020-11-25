97df84e838cc5d99cb2a51efba87c3f63e5afbfc
224441 Szymańska Julia <224441@edu.p.lodz.pl>
2020-11-25 17:38:56 +0100

    Added router

--- FrontEnd/src/App.vue
+++ FrontEnd/src/App.vue
@@ -1,24 +1,10 @@
 <template>
-<!--  <img alt="Vue logo" src="./assets/logo.png">-->
-<!--  <HelloWorld msg="Welcome to Your Vue.js App"/>-->
-  <LoginForm/>
-  <ChatsList/>
+  <div id="nav">
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link>
+  </div>
+  <router-view/>
 </template>
-
-<script>
-// import HelloWorld from './components/HelloWorld.vue'
-import ChatsList from "@/components/ChatsList";
-import LoginForm from "@/components/LoginForm";
-
-export default {
-  name: 'App',
-  components: {
-    // HelloWorld,
-    ChatsList,
-    LoginForm
-  }
-}
-</script>
 
 <style>
 #app {
@@ -27,6 +13,18 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+
+#nav {
+  padding: 30px;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
 }
 </style>
