9dd9c18127856345375dd34d288e2a6b0083787b
224466 Zdrzalik Przemyslaw <57731778+zdrzalik-przemek@users.noreply.github.com>
2020-11-25 17:02:04 +0100

    added login form, need make mult sites

---
+++ FrontEnd/src/components/LoginForm.vue
@@ -0,0 +1,48 @@
+<template>
+  <div>
+    <h2>Login</h2>
+    <form @submit.prevent="handleSubmit">
+      <div class="form-group">
+        <label for="username">Username</label>
+        <input
+          type="text"
+          v-model="username"
+          name="username"
+          class="form-control"
+        />
+        <div v-show="submitted && !username" class="invalid-feedback">
+          Username is required
+        </div>
+      </div>
+      <div class="form-group">
+        <label htmlFor="password">Password</label>
+        <input
+          type="password"
+          v-model="password"
+          name="password"
+          class="form-control"
+          :class="{ 'is-invalid': submitted && !password }"
+        />
+        <div v-show="submitted && !password" class="invalid-feedback">
+          Password is required
+        </div>
+      </div>
+      <div class="form-group">
+        <button class="btn btn-primary">Login</button>
+      </div>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "LoginForm",
+  data() {
+    return {
+      username: "",
+      password: "",
+      submitted: false,
+    };
+  },
+};
+</script>
