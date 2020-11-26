fde81291c457b37345113ce08fcf10923dc87af1
224441 Szymańska Julia <224441@edu.p.lodz.pl>
2020-11-26 19:15:32 +0100

    Added https

--- FrontEnd/src/main.js
+++ FrontEnd/src/main.js
@@ -1,7 +1,9 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import vueResource from 'vue-resource'
 
+createApp(App).use(vueResource)
 createApp(App).use(router).mount('#app')
 router.replace({ path: '/login', redirect: '/' })
 export default router
