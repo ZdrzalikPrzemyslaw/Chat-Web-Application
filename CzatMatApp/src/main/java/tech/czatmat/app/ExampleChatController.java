d2114f65b4d792b434dca230870f6e57d51ecea3
224441 Szymańska Julia <224441@edu.p.lodz.pl>
2020-11-26 18:13:55 +0100

    First responsE

--- CzatMatApp/src/main/java/tech/czatmat/app/ExampleChatController.java
+++ CzatMatApp/src/main/java/tech/czatmat/app/ExampleChatController.java
@@ -1,22 +1,21 @@
 package tech.czatmat.app;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 
 @RestController
 public class ExampleChatController {
 
-    @RequestMapping("/przyklad")
-    public List<Map<String, String>> index() {
-        List<Map<String, String>> lista = null;
-        lista = null;
-        lista = null;
-        Map<String, String> map = null;
-        return lista;
+    @RequestMapping(value = "/przyklad", method = RequestMethod.GET, produces = "application/json")
+    public List<Pair<String, String>> index() {
+        List<Pair<String, String>> list = new ArrayList<>();
+        Pair<String, String> pair = Pair.of("Julia", "Masakra");
+        list.add(pair);
+        return list;
     }
 
 }
---
+++ CzatMatApp/src/main/java/tech/czatmat/app/Pair.java
@@ -0,0 +1,26 @@
+package tech.czatmat.app;
+
+public class Pair<T, T1> {
+
+    public final T first;
+    public final T1 second;
+
+
+    private Pair(T first, T1 second){
+        this.first = first;
+        this.second = second;
+    }
+
+    public T getFirst() {
+        return first;
+    }
+
+    public T1 getSecond() {
+        return second;
+    }
+
+    public static <T, T1> Pair <T, T1> of(T a, T1 b){
+        return new Pair<>(a, b);
+    }
+
+}
