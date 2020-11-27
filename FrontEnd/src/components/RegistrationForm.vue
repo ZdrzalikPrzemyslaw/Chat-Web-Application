05f32039ce5b826b246c697dfac8c20a24349901
224466 Zdrzalik Przemyslaw <57731778+zdrzalik-przemek@users.noreply.github.com>
2020-11-27 16:24:43 +0100

    go to registration button

---
+++ FrontEnd/src/components/RegistrationForm.vue
@@ -0,0 +1,64 @@
+<template>
+  <div id="login">
+    <h2>Login</h2>
+    <form @submit.prevent="handleSubmit">
+      <div class="form-group sm-3">
+        <label for="username">Username</label>
+        <input
+          type="text"
+          v-model="username"
+          name="username"
+          class="form-control sm-3"
+        />
+        <div v-show="submitted && !username" class="invalid-feedback">
+          Username is required
+        </div>
+      </div>
+      <div class="form-group">
+        <label htmlFor="password">Password</label>
+        <input
+          type="password"
+          v-model="password"
+          name="password"
+          class="form-control sm-3"
+          :class="{ 'is-invalid': submitted && !password }"
+        />
+        <div v-show="submitted && !password" class="invalid-feedback">
+          Password is required
+        </div>
+      </div>
+      <div class="form-group">
+        <button class="btn btn-primary" v-on:click="this.$router.push('/')">
+          Login
+        </button>
+      </div>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "LoginForm",
+  data() {
+    return {
+      username: "",
+      password: "",
+      name: "",
+      surname: "",
+      email: "",
+      submitted: false,
+    };
+  },
+};
+</script>
+
+<style scoped>
+#login {
+  width: 20%;
+  margin: auto;
+  border: 1px solid gray;
+  border-radius: 10px;
+  padding: 10px 30px;
+  background: #dcdcdc;
+}
+</style>
---
+++ FrontEnd/src/views/Registration.vue
@@ -0,0 +1,24 @@
+<template>
+  <RegistrationForm  class="justify-center"/>
+</template>
+
+<script>
+import RegistrationForm from "../components/RegistrationForm";
+export default {
+  name: 'Registration',
+  components: {
+    RegistrationForm
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
