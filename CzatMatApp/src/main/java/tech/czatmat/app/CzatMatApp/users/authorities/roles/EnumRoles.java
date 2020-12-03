3e1e0a093ae0e445c9384a3b594d9a742c80d89b
224441 Szymańska Julia <224441@edu.p.lodz.pl>
2020-12-03 16:34:46 +0100

    deleted enum and added class to it

--- CzatMatApp/src/main/java/tech/czatmat/app/CzatMatApp/users/authorities/roles/EnumRoles.java
+++
@@ -1,7 +0,0 @@
-package tech.czatmat.app.CzatMatApp.users.authorities.roles;
-
-public enum EnumRoles {
-    ROLE_USER,
-    ROLE_MODERATOR,
-    ROLE_ADMIN
-}
--- CzatMatApp/src/main/java/tech/czatmat/app/CzatMatApp/users/authorities/roles/RoleSource.java
+++ CzatMatApp/src/main/java/tech/czatmat/app/CzatMatApp/users/authorities/roles/RoleSource.java
@@ -1,11 +1,20 @@
-//package tech.czatmat.app.CzatMatApp.users.authorities.roles;
-//
-//public class RoleSource {
-//
-//    public static RoleSource =;
-//
-//    public static RoleSource getInstance() {
-//        return ;
-//    }
-//
-//}
+package tech.czatmat.app.CzatMatApp.users.authorities.roles;
+
+public class RoleSource {
+
+    private RoleSource() {
+    }
+
+    public static String getRoleUser() {
+        return "ROLE_USER";
+    }
+
+    public static String getRoleSuperUser() {
+        return "ROLE_SUPER_USER";
+    }
+
+    public static String getRoleAdmin() {
+        return "ROLE_ADMIN";
+    }
+
+}
