9a9ac413d363c86e72dccc3579c08fd2eb10f0bb
Hubert Gawlowski <hubertgaw@gmail.com>
2020-12-08 00:57:16 +0100

    Add vee-validate and vuex libraries

--- FrontEnd/src/main.js
+++ FrontEnd/src/main.js
@@ -1,10 +1,13 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+// import store from './store'
+import VeeValidate from 'vee-validate'
 
 // import vueResource from 'vue-resource'
 
+
 // createApp(App).use(vueResource)
-createApp(App).use(router).mount('#app')
+createApp(App).use(router,VeeValidate).mount('#app')
 // router.replace({ path: '/login', redirect: '/' })
 export default router
