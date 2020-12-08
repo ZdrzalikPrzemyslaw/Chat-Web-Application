0bf2ad4585593cc5a09ac44eeeb17fe2ace4a0b9
Hubert Gawlowski <hubertgaw@gmail.com>
2020-12-08 01:26:31 +0100

    Create User Service

---
+++ FrontEnd/src/services/user.service.js
@@ -0,0 +1,24 @@
+import axios from 'axios';
+import authHeader from "./auth-header";
+
+class UserService {
+    getPublicContent() {
+        return axios.get(process.env.VUE_APP_BACKEND_URL + 'all');
+    }
+
+    getUserContent() {
+        return axios.get(process.env.VUE_APP_BACKEND_URL + 'user',
+            {headers: authHeader()});
+    }
+    // Tutaj dodajemy jak bedziemy mieli jakies strony, na ktore moga wejsc admini:
+    // przyklad ze strony https://bezkoder.com/jwt-vue-vuex-authentication/:
+    // getModeratorBoard() {
+    //     return axios.get(API_URL + 'mod', { headers: authHeader() });
+    // }
+    //
+    // getAdminBoard() {
+    //     return axios.get(API_URL + 'admin', { headers: authHeader() });
+    // }
+}
+
+export default new UserService();
