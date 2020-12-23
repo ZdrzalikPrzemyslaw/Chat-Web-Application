164e3a1bbba1cb69a2ac1122ef417b4b02d04951
Hubert Gawlowski <224298@edu.p.lodz.pl>
2020-12-24 00:01:59 +0100

    install yup, import vee-validate and yup

--- FrontEnd/src/components/LoginForm.vue
+++ FrontEnd/src/components/LoginForm.vue
@@ -41,11 +41,13 @@
 </template>
 <!--todo adjust to vee-validate 4 (vee-validate 4 works with vue 3, previous no)
   https://vee-validate.logaretm.com/v4/  https://vee-validate.logaretm.com/v4/examples/checkboxes-and-radio
-  https://jasonwatmore.com/post/2020/10/01/vue-3-veevalidate-form-validation-example-->
+  https://jasonwatmore.com/post/2020/10/01/vue-3-veevalidate-form-validation-example -->
 <script>
 //import axios from "axios";
 import User from '../models/user';
 import store from '../store/index';
+import { Form, Field } from 'vee-validate';
+import * as Yup from 'yup';
 
 export default {
   name: "LoginForm",
