d5df57c3080a0c3951443090767878bfb27d7f37
JuliaSzymanska <szymanska.julia.it@gmail.com>
2020-12-28 17:39:17 +0100

    Added api to url

---
+++ CzatMatApp/src/main/java/tech/czatmat/app/CzatMatApp/controllers/SearchUsersController.java
@@ -0,0 +1,13 @@
+package tech.czatmat.app.CzatMatApp.controllers;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/search")
+public class SearchUsersController {
+
+    
+
+}
