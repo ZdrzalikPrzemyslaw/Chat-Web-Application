271679456bcf1350f3d79989121e3d6b1e64725a
JuliaSzymanska <szymanska.julia.it@gmail.com>
2020-12-29 16:24:43 +0100

    Deleted comments

--- CzatMatApp/src/main/java/tech/czatmat/app/CzatMatApp/controllers/SearchUsersController.java
+++ CzatMatApp/src/main/java/tech/czatmat/app/CzatMatApp/controllers/SearchUsersController.java
@@ -5,13 +5,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
 import tech.czatmat.app.CzatMatApp.dataClasses.users.UserRepository;
 import tech.czatmat.app.CzatMatApp.payload.request.SearchNameSurnameRequest;
 import tech.czatmat.app.CzatMatApp.payload.response.SearchMultipleUsersResponse;
-import tech.czatmat.app.CzatMatApp.security.JwtUtils;
 
 @RestController
 @RequestMapping("/search")
@@ -27,12 +24,12 @@ public class SearchUsersController {
         this.userRepository = userRepository;
     }
 
-    @PreAuthorize("hasRole('ADMIN')")
-    @RequestMapping(value = "", method = RequestMethod.GET, produces = "application/json")
-    public ResponseEntity<?> getAllUsers() {
-        UserDetails userDetails = (UserDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-        return ResponseEntity.ok(new SearchMultipleUsersResponse(userRepository.findAll()));
-    }
+//    @PreAuthorize("hasRole('ADMIN')")
+//    @RequestMapping(value = "", method = RequestMethod.GET, produces = "application/json")
+//    public ResponseEntity<?> getAllUsers() {
+//        UserDetails userDetails = (UserDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+//        return ResponseEntity.ok(new SearchMultipleUsersResponse(userRepository.findAll()));
+//    }
 
     @PreAuthorize("hasRole('USER')")
     @RequestMapping(value = "", method = RequestMethod.GET, produces = "application/json")
