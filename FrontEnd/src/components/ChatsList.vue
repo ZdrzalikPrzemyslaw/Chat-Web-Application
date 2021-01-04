c01904369023656d3e41dc70e363be8e7e1e8eef
JuliaSzymanska <szymanska.julia.it@gmail.com>
2021-01-04 10:02:48 +0100

    Added emitting chat id after click

--- FrontEnd/src/components/ChatsList.vue
+++ FrontEnd/src/components/ChatsList.vue
@@ -1,6 +1,12 @@
 <template>
   <div class=".container" id="main_container">
-    <div class="col sm-3" v-for="chat in this.chats" v-bind:key="chat" id="OneChat" @click="this.onClickChat(chat.chatId)">
+    <div
+      class="col sm-3"
+      v-for="chat in this.chats"
+      v-bind:key="chat"
+      id="OneChat"
+      @click="this.onClickChat(chat.chatId)"
+    >
       <div class="row" id="UserName">
         {{ chat.chatName }}
       </div>
@@ -15,19 +21,22 @@
 export default {
   name: "ChatsList",
   props: {
-    chats: Array
+    chats: Array,
   },
   data() {
-    return {
-    }
+    return {};
   },
   methods: {
     onClickChat: function (chatId) {
-        self.$emit("search-event", chatId);
-        console.log("event emitted chat id to display");
-    }
-  }
-}
+      self.$emit("search-event", chatId);
+      console.log("event emitted chat id to display");
+    },
+  },
+  created: function () {
+    self.$emit("search-event", this.chats[0].chatId);
+    console.log("event emitted chat id to display");
+  },
+};
 </script>
 
 <style scoped>
@@ -36,7 +45,7 @@ export default {
   text-align: left;
   cursor: pointer;
   width: 100%;
-  background: #D4D4D4;
+  background: #d4d4d4;
   margin: 10px 5px 5px 50px;
   border-radius: 10px;
   color: black;
@@ -52,5 +61,4 @@ export default {
   opacity: 0.9;
   margin-left: 5px;
 }
- 
 </style>
