e3fdbddd522c3feaf399fcd84fb7c537e5460d33
JuliaSzymanska <szymanska.julia.it@gmail.com>
2021-01-16 17:08:14 +0100

    Changed button type in sendMessage

--- FrontEnd/src/components/SendMessage.vue
+++ FrontEnd/src/components/SendMessage.vue
@@ -11,7 +11,7 @@
       </div>
       <div class="col-md-2" id="sendButton">
         <button
-          type="submit"
+          type="button"
           class="btn btn-primary"
           v-on:click="sendMessage()"
         >
